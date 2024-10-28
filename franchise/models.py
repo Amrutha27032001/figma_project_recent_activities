406f7e321202f8e9125cfe7bb8a919806e4d7555
amrutha <amruthapk675@gmail.com>
2024-10-28 06:36:37 +0530

    changes

--- franchise/models.py
+++ franchise/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class RecentActivity(models.Model):
+    # Define the fields for RecentActivity here
+    action = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # Add other fields as required
+
+    def __str__(self):
+        return self.action
 # Create your models here.
